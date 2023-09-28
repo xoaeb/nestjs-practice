08fd1bbe01c8eaa457b7c30d517392cc82c51ac0
Shoaib Salim <shoaib100aib@gmail.com>
2023-09-28 22:21:53 +0530

    added postgres connection

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,9 +1,23 @@
 import { MessagesModule } from 'src/messages/messages.module';
 import { Module } from '@nestjs/common';
 import { TaskModule } from 'src/task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule, TaskModule],
+  imports: [
+    MessagesModule,
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-mgmt',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class MainModule {}
 //hi
--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,14 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { STATUS } from '../dto/create-task.dto';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  description: string;
+  @Column()
+  title: string;
+  @Column()
+  status: STATUS;
+}
